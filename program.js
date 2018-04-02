332dbe31f766ae49b09ff5af8f730907c91f9317
Aleksander1105 <matyjewiczaleksander@gmail.com>
2018-04-02 12:41:13 +0200

    index.html added and program.js modified

--- program.js
+++ program.js
@@ -1,13 +1,13 @@
 process.stdin.setEncoding('utf-8');
-process.stdin.on('readable', function {
+process.stdin.on('readable', function() {
 	var input = process.stdin.read();
 	if(input !== null) {
 		var instruction = input.toString().trim();
-		if(instruction ==='/exit') {
+		if(instruction === '/exit') {
 			process.stdout.write('Quitting app!\n');
 			process.exit();
 		} else {
-			process.stdout.write('Wrong instruction!\n');
+			process.stderr.write('Wrong instruction!\n');
 		}
 	}
 	 
