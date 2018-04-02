bad92785cd5ea88c9275b929cfa5d896ae742c95
Aleksander1105 <matyjewiczaleksander@gmail.com>
2018-04-02 17:43:01 +0200

    Working on the project

--- app/program.js
+++ app/program.js
@@ -1,5 +1,3 @@
-var os = require('os');
-
 process.stdin.setEncoding('utf-8');
 process.stdin.on('readable', function() {
 	var input = process.stdin.read();
@@ -17,7 +15,7 @@ process.stdin.on('readable', function() {
 				process.stdout.write('Process language is as following: ' + process.env.LANG + ' \n');
 				break;
 			case '/getOSinfo':
-				getOSinfo();
+				var OSinfo = require('../modules/OSinfo');
 				break;
 			default:
 				process.stderr.write('Wrong instruction!\n');
@@ -26,21 +24,3 @@ process.stdin.on('readable', function() {
 	 
 })
 
-function getOSinfo() {
-	var type = os.type();
-	if(type === 'Darwin') {
-		type = 'OSX';
-	} else if(type === 'Windows_NT'){
-		type = 'Windows';
-	}
-	var release = os.release();
-	var cpu = os.cpus()[0].model;
-	var uptime = os.uptime();
-	var userInfo = 	os.userInfo();
-	console.log('System: ', type);
-	console.log('Release: ', release);
-	console.log('CPU model: ', cpu);
-	console.log('Uptime: ', (uptime / 60).toFixed(0), 'min');
-	console.log('User name: ', userInfo.username);
-	console.log('Home dir: ', userInfo.homedir);
-}
